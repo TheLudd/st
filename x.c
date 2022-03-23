2e120b39fe263a97a003973e3695c54331b972d1
Ludwig Magnusson <ludwig@mediatool.com>
2022-03-23 09:01:45 +0100

    apply st-defaultfontsize-20210225-4ef0cbd.diff

--- x.c
+++ x.c
@@ -4,6 +4,7 @@
 #include <limits.h>
 #include <locale.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <sys/select.h>
 #include <time.h>
 #include <unistd.h>
@@ -1264,7 +1265,7 @@ xinit(int cols, int rows)
 		die("could not init fontconfig.\n");
 
 	usedfont = (opt_font == NULL)? font : opt_font;
-	xloadfonts(usedfont, 0);
+	xloadfonts(usedfont, defaultfontsize);
 
 	/* spare fonts */
 	xloadsparefonts();
@@ -2190,6 +2191,11 @@ main(int argc, char *argv[])
 		break;
 	case 'v':
 		die("%s " VERSION "\n", argv0);
+		break;
+	case 'z':
+		defaultfontsize = strtod(EARGF(usage()), NULL);
+		if (!(defaultfontsize > 0))
+			usage();
 		break;
 	default:
 		usage();
